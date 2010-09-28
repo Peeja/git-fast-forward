b6400792f7a80056b8c3f060f16d4547d4626312
Peter Jaros <peter.a.jaros@gmail.com>
2010-09-28 19:40:32 -0400

    Failure case: when the destination is not a descendent.

--- spec/git-fast-forward_spec.rb
+++ spec/git-fast-forward_spec.rb
@@ -44,20 +44,52 @@ describe "git fast-forward" do
         git add new-file
         git commit -m "Adding new-file."
       SH
-
-      @descendent_commit = commit_named("descendent-branch")
     end
 
     it "sets the current branch to the descendent commit" do
+      descendent_commit = commit_named("descendent-branch")
+
       run <<-SH
         git checkout parent-branch
         git fast-forward descendent-branch
       SH
 
-      commit_named("parent-branch").should == @descendent_commit
+      commit_named("parent-branch").should == descendent_commit
     end
   end
 
-  context "the given commit is not a descendent of the current branch"
+  context "the given commit is not a descendent of the current branch" do
+    before do
+      run <<-SH
+        touch initial-file
+        git add initial-file
+        git commit -m "Initial commit."
+        git checkout -b first-branch
+
+        git branch other-branch
+        touch second-file
+        git add second-file
+        git commit -m "Second commit."
+        
+        git checkout other-branch
+        touch new-file
+        git add new-file
+        git commit -m "Adding new-file."
+      SH
+    end
+
+    it "exists with a status of 1 and leaves the branch where it is" do
+      first_branch_before_ff = commit_named("first-branch")
+
+      run <<-SH
+        git checkout first-branch
+        git fast-forward other-branch
+      SH
+
+      $?.exitstatus.should == 1
+      commit_named("first-branch").should == first_branch_before_ff
+    end
+  end
+
   context "there is no current branch"
 end
